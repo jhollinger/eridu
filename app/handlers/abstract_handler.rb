3831bc022802723959623d20b07c69df70822364
Jordan Hollinger <jordan.hollinger@gmail.com>
2012-07-28 15:41:58 -0400

    Reorganize

--- app/handlers/abstract_handler.rb
+++ app/handlers/abstract_handler.rb
@@ -12,24 +12,4 @@ class AbstractHandler < Sinatra::Base
     include Rack::Recaptcha::Helpers if Conf[:recaptcha]
     alias_method :h, :escape_html
   end
-
-  before do
-    params.nilify! unless request.get?
-  end
-
-  configure :production, :development do
-    enable :logging
-  end
-
-  configure :production do
-    not_found do
-      erb :"errors/404"
-    end
-
-    error do
-      mail Conf[:author, :email], 'Eridu Exception', :exception if Conf[:mail, :errors]
-      status 500
-      erb :"errors/500"
-    end
-  end
 end
---
+++ app/handlers/config_handler.rb
@@ -0,0 +1,27 @@
+# Config for the main app (doesn't belong in AbstractHandler, because it only needs to be defined once)
+class ConfigHandler < AbstractHandler
+  use Rack::MethodOverride
+  use Rack::Session::Cookie, :key => 'eridu.session', :httponly => true
+  use Rack::OpenID
+  use Rack::Recaptcha, :public_key => Conf[:recaptcha, :public_key], :private_key => Conf[:recaptcha, :private_key] if Conf[:recaptcha]
+
+  before do
+    params.nilify! unless request.get?
+  end
+
+  configure :production, :development do
+    enable :logging
+  end
+
+  configure :production do
+    not_found do
+      erb :"errors/404"
+    end
+
+    error do
+      mail Conf[:author, :email], 'Eridu Exception', :exception if Conf[:mail, :errors]
+      status 500
+      erb :"errors/500"
+    end
+  end
+end
