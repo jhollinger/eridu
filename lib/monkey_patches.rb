cb494af3d3e058437967cdfa27a7dad98dab4dc0
Jordan Hollinger <jordan.hollinger@gmail.com>
2012-07-30 17:21:46 -0400

    Remove try

--- lib/monkey_patches.rb
+++ lib/monkey_patches.rb
@@ -1,12 +1,4 @@
 class Object
-  # Tentatively calls the method, returning nil if it doesn't exist.
-  # E.g. "obj.try(:foo).try(:bar).tar(:zoop)" or "obj.try(:foo, :bar, :zoop)"
-  def try(*methods)
-    methods.inject(self) do |obj, method|
-      obj.respond_to?(method) ? obj.send(method) : nil
-    end
-  end
-
   # Returns true for '', nil, [] and {}
   def blank?
     respond_to?(:empty?) ? empty? : !self
