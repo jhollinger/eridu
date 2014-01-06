e6a20dc132f522f4e2b435b028e3f1d1cdaa5b5f
Jordan Hollinger <jordan.hollinger@gmail.com>
2014-01-06 12:41:16 -0500

    Remove jquery from front-end

--- public/javascripts/lib.js
+++ public/javascripts/lib.js
@@ -1,7 +1,8 @@
-$(function() {
-  // De-obfuscate email addresses
-  $('.obfuscated').each(function() {
-    this.innerHTML = this.innerHTML.replace(' at ', '@').replace(' dot ', '.');
-    this.setAttribute('href', 'mailto:' + this.innerHTML);
-  })
-})
+window.onload = function() {
+  var obfuscated = document.getElementsByClassName('obfuscated');
+  for ( var i=0; i < obfuscated.length; i++  ) {
+    var el = obfuscated[i];
+    el.innerHTML = el.innerHTML.replace(' at ', '@').replace(' dot ', '.');
+    el.setAttribute('href', 'mailto:' + el.innerHTML);
+  }
+};
